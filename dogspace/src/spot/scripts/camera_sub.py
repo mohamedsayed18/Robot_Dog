a7e2bd64603ed6319a4c435b344d1e6b21f47e41
Mohamed_Ahmed <mohamed95.a.s@gmail.com>
2019-11-11 20:15:35 +0300

    Camera

---
+++ dogspace/src/spot/scripts/camera_sub.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import roslib
+#roslib.load_manifest('my_package')
+import sys
+import rospy
+import cv2
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+class image_converter:
+
+  def __init__(self):
+    #self.image_pub = rospy.Publisher("image_topic_2",Image)
+    self.counter = 0
+    self.bridge = CvBridge()
+    self.image_sub = rospy.Subscriber("image_raw",Image,self.callback)
+
+  def callback(self,data):
+    try:
+      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+      cv2.imwrite('/media/mohamed/C03CCDB43CCDA62E1/tutorials/Innopolis/1st/FRP/HA/Robot_Dog/images/image'+str(self.counter)+'.png', cv_image)
+      self.counter += 1
+      rospy.sleep(10.)
+    except CvBridgeError as e:
+      print(e)
+'''
+    try:
+      self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+    except CvBridgeError as e:
+      print(e)
+'''
+def main(args):
+  ic = image_converter()
+  rospy.init_node('image_converter', anonymous=True)
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("Shutting down")
+  cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
