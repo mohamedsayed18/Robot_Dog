a029c06496bef7b72ea1bd1b075cffd29b02f165
Mohamed_Ahmed <mohamed95.a.s@gmail.com>
2019-11-23 15:20:13 +0300

    travis_try

---
+++ dogspace/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gmock
+# Build directory: /media/mohamed/C03CCDB43CCDA62E1/tutorials/Innopolis/1st/FRP/HA/Robot_Dog/dogspace/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
